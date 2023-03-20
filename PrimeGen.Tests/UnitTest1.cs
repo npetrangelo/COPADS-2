85a3fc1550e05f1231aa58a782b0162422e7eaf7
Nathan Petrangelo <nathan@petrangelo.org>
2023-03-20 12:50:11 -0400

    Finished IsProbablyPrime()

--- PrimeGen.Tests/UnitTest1.cs
+++ PrimeGen.Tests/UnitTest1.cs
@@ -1,14 +1,26 @@
-using PrimeGen;
+using System.Numerics;
 
 namespace PrimeGen.Tests;
 
 public class Tests {
     [SetUp]
     public void Setup() {
+        Program.bits = 8;
     }
 
     [Test]
     public void Test1() {
         Assert.Pass();
+    }
+
+    [Test]
+    public void TestIsIntProbablyPrime() {
+        Assert.True(Program.IsIntProbablyPrime(97));
+        Assert.False(Program.IsIntProbablyPrime(100));
+    }
+    
+    [Test]
+    public void TestIsProbablyPrime() {
+        Assert.True(new BigInteger(97).IsProbablyPrime());
     }
 }
--- PrimeGen/Program.cs
+++ PrimeGen/Program.cs
@@ -6,11 +6,13 @@ using System.Security.Cryptography;
 namespace PrimeGen;
 
 static class Program {
+    public static int bits;
     static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
+        bits = Int32.Parse(args[0]);
     }
 
-    static Boolean IsProbablyPrime(this BigInteger value, int k = 10) {
+    public static Boolean IsProbablyPrime(this BigInteger value, int k = 10) {
         if (value > 0 && value < 3) { // Handle base case
             return true;
         }
@@ -27,9 +29,67 @@ static class Program {
         }
         
         for (int i = 0; i < k; i++) {
-            var a = RandomNumberGenerator.GetInt32(2, BigInteger.Subtract(value, 2));
-            var bytes = RandomNumberGenerator.GetBytes(3); // How many bytes?
-            var rand = new BigInteger(bytes);
+            var bytes = RandomNumberGenerator.GetBytes(bits/8);
+            var a = new BigInteger(bytes);
+            // Try again until random number is in valid range
+            while (a < 2 || BigInteger.Compare(a, BigInteger.Subtract(value, 2)) > 0) {
+                bytes = RandomNumberGenerator.GetBytes(bits/8);
+                a = new BigInteger(bytes);
+            }
+            var x = BigInteger.ModPow(a, d, value);
+            if (x == 1 || BigInteger.Compare(x, BigInteger.Subtract(value, 1)) == 0) {
+                continue;
+            }
+
+            var y = BigInteger.ModPow(x, 2, value);
+            for (int j = 0; j < r; j++) {
+                y = BigInteger.ModPow(x, 2, value);
+                if (y == 1 && x != 1 && BigInteger.Compare(x, BigInteger.Subtract(value, 1)) != 0) {
+                    return false;
+                }
+                x = y;
+            }
+            if (y != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static Boolean IsIntProbablyPrime(int value, int k = 10) {
+        if (value > 0 && value < 3) { // Handle base case
+            return true;
+        }
+
+        if (value % 2 == 0) {
+            return false;
+        }
+
+        var d = value - 1;
+        var r = 0;
+        while (d % 2 == 0) {
+            d /= 2;
+            r++;
+        }
+        
+        for (var i = 0; i < k; i++) {
+            var a = new Random().Next(2, value - 1);
+            var x = (int) Math.Pow(a, d) % value;
+            if (x == 1 || x == value - 1) {
+                continue;
+            }
+
+            var y = x*x % value;
+            for (var j = 0; j < r; j++) {
+                y = x*x % value;
+                if (y == 1 && x != 1 && x != value - 1) {
+                    return false;
+                }
+                x = y;
+            }
+            if (y != 1) {
+                return false;
+            }
         }
         return true;
     }
